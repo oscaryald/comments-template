ff2dc604efa3f7095fb7330d2df2e2e2b57e964e
Aleksandr.Trokhimenk <aleksandr.trokhimenko@devellar.com>
2017-10-17 21:33:21 +0300

    edit

--- gulpfile.js
+++ gulpfile.js
@@ -7,22 +7,12 @@ var gulp = require('gulp'); // Require gulp
 var sass = require('gulp-sass'); // Compile Sass into CSS
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
-// var minifyCSS = require('gulp-minify-css'); // Minify the CSS
 
-// Minification dependencies
-// var minifyHTML = require('gulp-minify-html'); // Minify HTML
-// var concat = require('gulp-concat'); // Join all JS files together to save space
-// var stripDebug = require('gulp-strip-debug'); // Remove debugging stuffs
-// var uglify = require('gulp-uglify'); // Minify JavaScript
 var imagemin = require('gulp-imagemin'); // Minify images
 
 // Other dependencies
 var size = require('gulp-size'); // Get the size of the project
 var browserSync = require('browser-sync'); // Reload the browser on file changes
-// var jshint = require('gulp-jshint'); // Debug JS files
-// var stylish = require('jshint-stylish'); // More stylish debugging
-
-var spritesmith = require('gulp.spritesmith');
 
 var gulpSequence = require('gulp-sequence');
 var newer = require('gulp-newer');
@@ -46,22 +36,6 @@ gulp.task('styles', function() {
     }));
 });
 
-// Task to minify new or changed HTML pages
-// gulp.task('html', function() {
-//   gulp.src('./app/*.html')
-//     .pipe(minifyHTML())
-//     .pipe(gulp.dest('./build/'));
-// });
-
-// Task to concat, strip debugging and minify JS files
-// gulp.task('scripts', function() {
-//   gulp.src(['./app/scripts/lib.js', './app/scripts/*.js'])
-//     .pipe(concat('script.js'))
-//     .pipe(stripDebug())
-//     .pipe(uglify())
-//     .pipe(gulp.dest('./build/scripts/'));
-// });
-
 // Task to minify images into build
 gulp.task('images', function() {
   gulp.src('./source/img/*')
@@ -70,53 +44,6 @@ gulp.task('images', function() {
   }))
   .pipe(gulp.dest('./build/img'));
 });
-
-// Task to sprites into build
-gulp.task('sprite', function () {
-
-  // return 
-  // var spriteData = gulp.src('source/img/icons/*.png') // путь, откуда берем картинки для спрайта
-  //           .pipe(spritesmith({
-  //               imgName: 'sprite.png',
-  //               cssName: 'sprite.css',
-  //               cssFormat: 'less',
-  //               algorithm: 'binary-tree',
-  //               // cssTemplate: 'sprite.less',
-  //               cssVarMap: function(sprite) {
-  //                     sprite.name = '@-s' + sprite.name
-  //               }
-  //           }));
-
-  //   spriteData.img.pipe(gulp.dest('dest/img/icons')); // путь, куда сохраняем картинку
-  //   spriteData.css.pipe(gulp.dest('dest/css')); // путь, куда сохраняем стили
-
-  return gulp.src('source/img/icons/*.png')
-          .pipe(spritesmith({
-              imgName: 'sprite.png',
-              cssName: 'sprite.scss',
-              cssFormat: 'scss',
-              // algorithm: 'binary-tree',
-              imgPath: '../img/sprite/sprite.png',
-              // cssTemplate: 'less.template.mustache',
-              cssVarMap: function(sprite) {
-                    sprite.name = 's-' + sprite.name
-              }
-          }))
-          .pipe(gulp.dest('source/img/sprite'))
-          // .pipe(gulp.dest('source/sass'))
-          .pipe(gulp.dest('build/img/sprite'));
-          // gulp.src('source/img/icons/*.png').img.pipe(gulp.dest('build/img/icons')); // путь, куда сохраняем картинку
-          // gulp.src('source/img/icons/*.png').css.pipe(gulp.dest('source/sass'));
-          
-          // .pipe(gulp.dest('build/css'))
-});
-
-// Task to run JS hint
-// gulp.task('jshint', function() {
-//   gulp.src('./source/js/*.js')
-//     .pipe(jshint())
-//     .pipe(jshint.reporter('jshint-stylish'));
-// });
 
 // Task to get the size of the app project
 gulp.task('size', function() {
@@ -148,7 +75,7 @@ gulp.task('cleanAll', function(){
         .pipe(clean()); 
 });
 
-gulp.task('build', gulpSequence('cleanAll', ['assets', 'styles', 'sprite', 'images', 'size']));
+gulp.task('build', gulpSequence('cleanAll', ['assets', 'styles', 'images', 'size']));
 
 // Serve application
 gulp.task('serve', function() {
@@ -160,81 +87,10 @@ gulp.task('serve', function() {
 });
 
 
-
 // Run all Gulp tasks and serve application
-gulp.task('run', ['build', 'serve'], function() {
+gulp.task('dev', ['build', 'serve'], function() {
   gulp.watch('source/sass/**/*.scss', ['styles']);
   gulp.watch('source/*.html', browserSync.reload);
   gulp.watch('source/js/*.js', browserSync.reload);
   gulp.watch('source/**/*.*', ['assets']);
-  // gulp.watch('source/js/**/*.js', browserSync.reload);
 });
-
-
-
-// start site
-
-// gulp.task('dev', gulpSequence('build', ['run', 'serve']));
-
-
-
-
-
-
-
-
-
-// var gulp = require('gulp');
-// var browserify = require('gulp-browserify');
-// var concat = require('gulp-concat');
-// var less = require('gulp-less');
-// var refresh = require('gulp-livereload');
-// var lr = require('tiny-lr');
-// var server = lr();
-// var minifyCSS = require('gulp-minify-css');
-// var embedlr = require('gulp-embedlr');
-
-// gulp.task('scripts', function() {
-//     gulp.src(['app/src/**/*.js'])
-//         .pipe(browserify())
-//         .pipe(concat('dest.js'))
-//         .pipe(gulp.dest('dist/build'))
-//         .pipe(refresh(server))
-// })
-
-// gulp.task('styles', function() {
-//     gulp.src(['app/css/style.less'])
-//         .pipe(less())
-//         .pipe(minifyCSS())
-//         .pipe(gulp.dest('dist/build'))
-//         .pipe(refresh(server))
-// })
-
-// gulp.task('lr-server', function() {
-//     server.listen(35729, function(err) {
-//         if(err) return console.log(err);
-//     });
-// })
-
-// gulp.task('html', function() {
-//     gulp.src("app/*.html")
-//         .pipe(embedlr())
-//         .pipe(gulp.dest('dist/'))
-//         .pipe(refresh(server));
-// })
-
-// gulp.task('default', function() {
-//     gulp.run('lr-server', 'scripts', 'styles', 'html');
-
-//     gulp.watch('app/src/**', function(event) {
-//         gulp.run('scripts');
-//     })
-
-//     gulp.watch('app/css/**', function(event) {
-//         gulp.run('styles');
-//     })
-
-//     gulp.watch('app/**/*.html', function(event) {
-//         gulp.run('html');
-//     })
-// })
--- source/js/main.js
+++ source/js/main.js
@@ -1,16 +1,9 @@
 $(document).ready(function(){
 
 	function Post(param){
-
 		this.countComments = param.countComments || 1;
-
 		this.loadMoreComments = param.loadMoreComments || 1;
-
 		this.items = [];
-
-		this.that = this
-
-		
 	}
 
 	Post.prototype = {
@@ -22,109 +15,120 @@ $(document).ready(function(){
 
 			this.getPost();
 
-			this.addEventHandler()
+			this.addEventHandler();
+
+			this.filterComment()
 
 		},
 
-		sortPost: function(arr, num){
-
-			$(arr).addClass(hidden)
-
-		},
 
 		loadMore: function(arr){
 
-			this.countComments = arr.length;
+			// this.countComments = arr.length;
 
 			if(this.countComments + this.loadMoreComments > this.countComments){
 				this.countComments += this.loadMoreComments
 			}
 
-			this.getPost({
-				pos: arr.length
-			})
+			this.getPost()
 			
 			return 
+		},
+
+		addCommentsTemplate : function(comment){
+			var that = this;
+			var commentsTemplate = $('#comments-template').html()
+			comment.created_at = that.dateReviver(comment.created_at)
+			for(var i = 0; i < comment.children.length; i++){
+				comment.children[i].created_at = that.dateReviver(comment.children[i].created_at)
+			}
+			$( ".comments-list" ).append(Mustache.render(commentsTemplate, comment));
 		},
 
 		addEventHandler: function(){
 
 			var that = this;
+			var valTextarea
+			var commentId
 
-			console.log(this.items.length)
+			// var valTextarea = $(this).parents('li').find('.comment-content').val();
+
+			// var commentId = $(this).parents('li').attr('id')
 
 			$('.load-more').on('click', function(){
 				that.loadMore(that.items);
+				return false
+			});
 
+			$('.comments-content').on('click','.edit', function(){
+				valTextarea = $(this).parents('li').find('.comment-content').val();
+				commentId = $(this).parents('li').attr('id');
+				if(valTextarea == ''){
+					$(this).parents('li').find('.comment-content').addClass('error');
+					return false
+				}else{
+					$(this).parents('li').find('.comment-content').removeClass('error');
+				} 
+				that.editComment.apply($(this), [valTextarea, commentId]);
+				$(this).parents('li').find('.comment-content').val('')
+				return false
+			});
+
+			$('.comments-content').on('click','.delete', function(){
+				commentId = $(this).parents('li').attr('id');
+				that.deleteComment.apply($(this), [commentId]);
 				return false
 			})
+
+
+			$('.comments-content').on('click','.reply', function(){
+				valTextarea = $(this).parents('li').find('.comment-content').val();
+				that.replayComment.apply($(this), [valTextarea]);
+				return false
+			})
+
+			$('.comments-block').on('click','.add', function(){
+				valTextarea = $(this).parents('.message-field').find('.comment-content').val();
+				console.log(valTextarea)
+				if(valTextarea == ''){
+					$(this).parents('.message-field').find('.comment-content').addClass('error');
+					return false
+				}else{
+					$(this).parents('.message-field').find('.comment-content').removeClass('error');
+				} 
+				that.addNewComment.apply($(this), [valTextarea]);
+				$(this).parents('.message-field').find('.comment-content').val('')
+				return false
+			});
+
+			$('.comments-block').on('click','.cancel', function(){
+				$(this).parents('.message-field__textarea').addClass('hidden');
+				return false
+			})
+
+
+		},
+
+		dateReviver : function(value){
+			var ms = Date.parse(value),
+				    date = new Date(ms),
+				    year = date.getFullYear(),
+				    month = date.getMonth()+1,
+				    day = date.getDate(),
+				    minutes = ( date.getMinutes()<=9 ) ? '0' + date.getMinutes() : date.getMinutes(),
+				    hours = date.getHours();
+				value = year + '-' + month + '-' + day + ' at ' + hours + ':' + minutes;
+			    return value;
 		},
 
 		getPost : function(param){
 
 			var that = this;
 
-			for(var i in param ){
-				var pos = param.pos
-			}
-
-			function dateReviver(key, value) {
-			    var ms = Date.parse(value),
-				    date = new Date(ms),
-				    year = date.getFullYear(),
-				    month = date.getMonth(),
-				    day = date.getDate(),
-				    minutes = ( date.getMinutes()<=9 ) ? '0' + date.getMinutes() : date.getMinutes();
-				    hours = date.getHours();
-				value = year + '-' + month + '-' + day + ' at ' + hours + ':' + minutes;
-			    return value;
-			};
-
-			function createTemplate(arr, key){
-
-				arr.push( "<li class='comment'>" + 
-				      	'<img src="'+this.author.avatar+'" alt="" />'+
-				      	'<h3>'+this.author.name+'</h3><span>'+dateReviver(key, this.author.created_at)+'</span>'+
-				      	'<p>'+this.content+'</p>'+
-				      	
-				      	'<ul>'+createChildrenTemplate.apply(this)+'</ul>'+
-				      	
-				      	createButtons()+
-				      	"</li>" );
-			}
-
-			function createChildrenTemplate(){
-
-				if(this.children.length == 0) return '';
-
-				var arrChildren = [];
-
-				this.children.forEach(function(el, key){
-					arrChildren.push( 
-				      	'<li><img src="'+el.author.avatar+'" alt="" />'+
-				      	'<h3>'+el.author.name+'</h3><span>'+dateReviver(key, el.author.created_at)+'</span>'+
-				      	'<p>'+el.content+'</p>'+'</li>'
-				      	 );
-				});
-				arrChildren = arrChildren.join('')
-				return arrChildren
-			}
-
-			function createButtons(){
-				var btnHtml = '<div class="btn-block">'+
-				'<a href="#" class="edit">Edit</a> '+
-				'<a href="#" class="delete">Delete</a> '+	
-				'<a href="#" class="reply">Reply</a> '+		
-				'</div>'
-				return btnHtml
-			}
-
-			
-
 			function load(){
 
-				$('.comments-content').html('');
-				that.items = []
+				$('.comments-list').html('');
+				that.items = [];
 
 				$.ajax({
 				    url: 'http://frontend-test.pingbull.com/pages/oscaryld@gmail.com/comments/',
@@ -136,25 +140,156 @@ $(document).ready(function(){
 					
 					    $.each( data, function( key, val ) {
 
-					    	if (key < that.countComments) {
-						          createTemplate.apply(val, [that.items, key]);
-						    }  
+						    if (key < that.countComments) {
+						    	that.addCommentsTemplate(val);
+							}
 
 					    });
-
-
-					    $( "<ul/>", {
-					      "class": "comments-list",
-					      html: that.items.join( "" )
-					    }).appendTo( ".comments-content" );
-
+						
 				    }
 				})	
 
 			};
 
-			return load()
+			load()
+
 		},
+
+		addNewComment : function(newComment){
+
+			var id = $(this).parents('li').attr('id');
+			var targetComment = $(this);
+			var comment = {
+				content : newComment,
+				id: id || null
+			}
+
+			$.ajax({
+				type: "POST",
+				url : 'http://frontend-test.pingbull.com/pages/oscaryld@gmail.com/comments/',
+				data:comment,
+				success: function(data){
+					var commentsTemplate = $('#comments-template').html();
+					data.created_at = Post.prototype.dateReviver.apply(Post, [data.created_at])
+					$( ".comments-list" ).prepend(Mustache.render(commentsTemplate, data));
+					console.log(data.content)
+				}
+
+			})
+
+			return false
+
+		},
+
+		replayComment : function(newComment){
+			var id = $(this).parents('li').attr('id');
+			var targetComment = $(this);
+
+
+			var url = 'http://frontend-test.pingbull.com/pages/oscaryld@gmail.com/comments/';
+
+			var comment = {
+
+				children : [{content : newComment}]
+				// 	{
+				// 		content : newComment,
+				// 		// id: id
+				// 	}
+				// ]
+			}
+
+			console.log(comment)
+
+			$.ajax({
+				type: "PATCH",
+				url : url + id,
+				data:comment,
+				success: function(data){
+
+					var commentsTemplate = $('#comments-template').html();
+					data.created_at = Post.prototype.dateReviver.apply(Post, [data.created_at])
+					$( ".comments-list" ).prepend(Mustache.render(commentsTemplate, data));
+					console.log(data.content)
+
+				}
+
+			})
+
+			return false
+
+		},
+
+		deleteComment : function(id){
+
+				var elementId = $('#'+id) 
+
+				var url = 'http://frontend-test.pingbull.com/pages/oscaryld@gmail.com/comments/';
+
+			    $.ajax({
+			         type: "DELETE",
+			         url: url + id,
+			         dataType: "json",
+			         success: function (data, status, jqXHR) {
+			            elementId.remove()
+			         },
+			     
+			         error: function (jqXHR, status) {
+			             // error handler
+			         }
+			     });
+
+			    return false
+
+		},
+
+		editComment : function(newComment, id){
+
+				var elementId = $('#'+id);
+
+				var comment = {
+					content : newComment
+				} 
+
+				var url = 'http://frontend-test.pingbull.com/pages/oscaryld@gmail.com/comments/';
+
+			    $.ajax({
+			         type: "PUT",
+			         url: url + id,
+			         dataType: "json",
+			         data:comment,
+			         success: function (data, status, jqXHR) {
+			         	elementId.find('p').html(newComment)
+			         },
+			     
+			         error: function (jqXHR, status) {
+			             // error handler
+			         }
+			     });
+
+			    return false
+		},
+
+		filterComment : function(id){
+
+				var id = '1';
+				var url = 'http://frontend-test.pingbull.com/pages/oscaryld@gmail.com/comments?filter[{"author":"id"}]';
+
+			    $.ajax({
+			         type: "GET",
+			         url: url,
+			         dataType: "json",
+			         success: function (data, status, jqXHR) {
+			         	console.log(url)
+			         	console.log(data)
+			         },
+			     
+			         error: function (jqXHR, status) {
+			             // error handler
+			         }
+			     });
+
+			    return false
+		}
 
 
 
@@ -163,11 +298,12 @@ $(document).ready(function(){
 
 
 	var post = new Post({
-		countComments:1,
-		loadMoreComments: 1
+		countComments:4,
+		loadMoreComments: 2
 	});
 
 
 	post.init()
+
 
 });
